e434dbd63e992748bd7caaad3f858dc04615ae91
Rick van Mook <rick@rvmook.com>
2016-08-05 19:26:05 -0400

    Initial project setup

---
+++ unity/Assets/Salsa/NetworkHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using BestHTTP;
+using BestHTTP.SocketIO;
+
+public class NetworkHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		var manager = new SocketManager(new Uri("http://localhost:3000/socket.io/"));
+
+		manager.Socket.On("chat", OnChat);
+	}
+	
+	void OnChat(Socket socket, Packet packet, params object[] args) {
+
+		Debug.Log(args[0]);
+	}
+}
