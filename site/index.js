0397d0cee4f4efbd48125485f4e677e6ddc8665d
Rick van Mook <rick@rvmook.com>
2016-08-06 22:10:01 -0400

    Initial setup for socket messaging

--- site/index.js
+++ site/index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var counter = 0;
+var connectedSalsas = {};
+var _canvasSocket;
 
 app.get('/', function(req, res){
 	res.sendfile('index.html');
@@ -8,19 +11,58 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 
-	console.log('a user connected');
+	socket.on('connectCanvas', function(){
 
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
+		console.log('connectCanvas');
+
+		_canvasSocket = socket;
+
+		socket.on('disconnect', function(){
+
+			_canvasSocket = false;
+		});
 	});
 
-	socket.on('chat', function(msg){
-		console.log('message: ' + msg);
-		io.emit('chat', msg);
+	socket.on('connectSalsa', function(){
+
+		console.log('connectSalsa');
+
+		var id = getUniqueId();
+		connectedSalsas[id] = {id:id};
+
+		io.to(socket.id).emit('salsaConnectedAs', id);
+		emitToCanvas('newSalsa', [id]);
+		socket.on('disconnect', function(){
+
+			console.log('disconnectSalsa', id);
+
+			emitToCanvas('fall', [id]);
+
+			delete connectedSalsas[id];
+		});
+
+		socket.on('rotate', function(angle){
+
+			console.log('rotate', angle);
+			emitToCanvas('rotate', [id, angle]);
+		});
 	});
-
-
 });
+
+function emitToCanvas(message, params) {
+
+	if(_canvasSocket) {
+
+		console.log('emitToCanvas', message, params);
+
+		io.to(_canvasSocket.id).emit.apply(io.to(_canvasSocket.id), [message].concat(params));
+	}
+}
+
+function getUniqueId() {
+
+	return String(counter++);
+}
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
--- unity/Assets/Salsa/NetworkHandler.cs
+++ unity/Assets/Salsa/NetworkHandler.cs
@@ -5,16 +5,54 @@ using BestHTTP.SocketIO;
 
 public class NetworkHandler : MonoBehaviour {
 
+	SocketManager manager;
+	
 	// Use this for initialization
 	void Start () {
 		
-		var manager = new SocketManager(new Uri("http://localhost:3000/socket.io/"));
+		Debug.Log("Start");
 
-		manager.Socket.On("chat", OnChat);
+		manager = new SocketManager(new Uri("http://localhost:3000/socket.io/"));
+
+
+		manager.Socket.On("connect", OnConnect);
+		manager.Socket.On("connect_error", OnConnectError);
+		manager.Socket.On("connect_timeout", OnConnectTimeout);
+
+
+		manager.Socket.On("fall", OnFall);
+		manager.Socket.On("rotate", OnRotate);
+		manager.Socket.On("newSalsa", OnNewSalsa);
+	}
+
+	void OnNewSalsa(Socket socket, Packet packet, params object[] args) {
+
+		Debug.Log("OnNewSalsa, id: " + args[0]);
 	}
 	
-	void OnChat(Socket socket, Packet packet, params object[] args) {
+	void OnFall(Socket socket, Packet packet, params object[] args) {
 
-		Debug.Log(args[0]);
+		Debug.Log("OnFall, id: " + args[0]);
+	}
+
+	void OnRotate(Socket socket, Packet packet, params object[] args) {
+
+		Debug.Log("OnRotate, id: " + args[0] + ", angle: " + args[1]);
+	}
+
+	void OnConnect(Socket socket, Packet packet, params object[] args) {
+
+		manager.Socket.Emit("connectCanvas");
+		Debug.Log("OnConnect");
+	}
+
+	void OnConnectError(Socket socket, Packet packet, params object[] args) {
+
+		Debug.Log("OnConnectError");
+	}
+
+	void OnConnectTimeout(Socket socket, Packet packet, params object[] args) {
+
+		Debug.Log("OnConnectTimeout");
 	}
 }
